b6e89d5be28bf1648df7b924ef8fc38b3f190b50
584219314 <36286501+584219314@users.noreply.github.com>
2018-02-09 10:40:51 +0800

    Add files via upload

---
+++ demo-all/demo-model/src/main/java/com/sasori/util/SasoriTest.java
@@ -0,0 +1,7 @@
+package com.sasori.util;
+
+public class SasoriTest {
+	public void test(){
+		System.out.println("lsz");
+	}
+}
---
+++ demo-all/demo-service/src/main/java/com/sasori/service/App.java
@@ -0,0 +1,16 @@
+package com.sasori.service;
+
+import com.sasori.util.SasoriTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SasoriTest sasori = new SasoriTest();
+    	sasori.test();
+    }
+}
